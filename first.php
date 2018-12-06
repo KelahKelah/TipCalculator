e890625c95ecc1c7298e44626f406799c7b2ee6f
KelahKelah <43876043+KelahKelah@users.noreply.github.com>
2018-12-06 00:07:47 -0800

    php updated
    cookies added...testing testing

--- first.php
+++ first.php
@@ -1,2 +1,24 @@
 <? php 
-echo my first php ?>
+echo my first php 
+
+
+$cookie_name = "user";
+$cookie_value = "Adaobi";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+?>
+<html>
+<body>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
+</body>
+</html>
+
+?>
